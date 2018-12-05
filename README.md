c6e298263dc2aac1bbcea5bf4ab0584a548d888e
Cody <bigbrocode@yahoo.com>
2018-12-05 15:09:44 -0500

    Added code from heroku

--- README.md
+++
@@ -1,4 +0,0 @@
-# DoodleCache
-
-This is my app for Senior Projects!
-It works with maps, drawing, and geo-location!
---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+
+var app = express();
+
+var server = app.listen(process.env.PORT || 5000);
+app.use(express.static('public'));
+
+console.log("My socket server is running");
+
+var socket = require('socket.io');
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+  console.log("new connection: ")
+  console.log(socket.id);
+
+  socket.on('mouse', mouseEvent);
+  socket.on('clear', clearCanvas);
+
+  function mouseEvent(data) {
+    data.id = socket.id;
+    socket.broadcast.emit('mouse', data);
+  }
+
+  function clearCanvas() {
+    socket.broadcast.emit('clear');
+  }
+}
